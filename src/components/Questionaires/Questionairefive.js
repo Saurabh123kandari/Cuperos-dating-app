16fe997c75dda61bddcc11c7340e0441bb3a5ce8
Monika <monika.recraft@gmail.com>
2022-05-10 13:40:17 +0530

    question five - six done

--- src/components/Questionaires/Questionairefive.js
+++ src/components/Questionaires/Questionairefive.js
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import { SafeAreaView,StyleSheet,View,Text,TouchableOpacity,Image} from 'react-native';
 import {ChevronLeftIcon,Progress, ScrollView} from 'native-base'
 import LinearGradient from 'react-native-linear-gradient';
 
 const Questionairefive = (props) => {
- 
-  
-    const plusicon = require('../../assets/Vector3.png');
+  const [data, setData] = useState([
+    {
+      id: 1,
+      text: 'Tennis',
+    },
+    {
+      id: 2,
+      text: 'Table Tennis',
+      
+    },
+    {
+      id: 3,
+      text: 'Handball',
+      
+    },
+    {
+      id: 4,
+      text: 'Hockey',
+      
+    },
+    {
+      id: 5,
+      text: 'Rowing',
+    },
+    {
+      id: 6,
+      text: 'Volleyball',
+    },
+    {
+      id: 7,
+      text: 'Basketball',
+    },
+    {
+      id: 8,
+      text: 'Soccer',
+    },
+    {
+      id: 9,
+      text: 'Golf',
+    },
+    {
+      id: 10,
+      text: 'Yoga/Pilates',
+    },
+    {
+      id: 11,
+      text: 'Badminton',
+    },
+    {
+      id: 12,
+      text: 'Fitness',
+    },
+    {
+      id: 13,
+      text: 'Cricket',
+    },
+    {
+      id: 14,
+      text: 'Fishing',
+    },
+  ]);
+const plusicon = require('../../assets/Vector3.png');
+const whitePlusIcon = require('../../assets/Frame.png')
+
+const handleClick = (item) =>{
+  const updateValues = data.map((content)=>{
+   if (item?.id === content?.id)
+   return {...content, check: !item?.check}
+   else return content;
+  })
+  setData(updateValues)
+ }
   return (
     <SafeAreaView style={styles.MainContainer}>
       <ScrollView>
@@ -25,163 +94,22 @@ const Questionairefive = (props) => {
           <Text style={styles.innertextt}>(Choose all that applies)</Text>
         </View>
         <View style={styles.view_two}>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Tennis</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#FFFFFF'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Table Tennis</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Handball</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Hocky</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#FFFFFF'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Rowing</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Volleyball</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View
-              style={[
-                styles.inner_view_two,
-                {backgroundColor: '#FF0000', borderColor: '#FFFFFF'},
-              ]}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={[styles.text_two, {color: '#FFFFFF'}]}>
-                Basketball
-              </Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Soccer</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Golf</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Yoga/Pilates</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Badminton</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Fitness</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View
-              style={[
-                styles.inner_view_two,
-                {backgroundColor: '#FF0000', borderColor: '#FFFFFF'},
-              ]}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#FFFFFF'}}
-                alt="Alternate Text"></Image>
-              <Text style={[styles.text_two, {color: '#FFFFFF'}]}>Cricket</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Image
-                source={plusicon}
-                style={{width: 14, height: 14, color: '#9D9D9D'}}
-                alt="Alternate Text"></Image>
-              <Text style={styles.text_two}>Fishing</Text>
-            </View>
-          </View>
+          {data.map(item => {
+            return (
+              <TouchableOpacity onPress={() => handleClick(item)}>
+              
+              <View style={item.check ? [styles.inner_view_two,{backgroundColor:'#FF0000',borderColor:'#FFFFFF'}] : styles.inner_view_two }>
+                    <Image
+                      source={item?.check ? whitePlusIcon : plusicon}
+                      style={{width: 14, height: 14, color: '#9D9D9D', marginTop:4}}
+                      alt="Alternate Text"></Image>
+                   <Text style={item.check ?  [styles.text_two,{color:'#ffff'}]:styles.text_two}>{item.text}</Text>
+                  </View>
+                 
+              </TouchableOpacity>
+            );
+          })}
+        
           <LinearGradient
             colors={['#D72D79', '#9264F2']}
             start={{x: 0, y: 0}}
@@ -201,84 +129,80 @@ const Questionairefive = (props) => {
   );
 }
 
-const styles=StyleSheet.create({
-    MainContainer:{
-    flex:1,
-    backgroundColor:'#FFFFFF'
-    },
-    icon_view_one: {
-        marginTop:10,
-        flex: 0.25,
-        //backgroundColor:'red'
-      },
-      innertext: {
-        fontSize: 22,
-        fontWeight: '500',
-        textAlign: 'center',
-        maxWidth:210,
-        alignSelf:'center'
-        
-      },
-      innertextt:{
-        fontSize: 22,
-        fontWeight: '500',
-        textAlign: 'center',
-        maxWidth:250,
-        alignSelf:'center'
-      },
-      view_two: {
-          marginTop:20,
-        flex: 0.75,
-        paddingHorizontal:20,
-       //backgroundColor: 'red',
-      },
-      inner_view_two: {
-        //backgroundColor: '#F8F8F8',
-        //width:'40%',
-        //flexWrap:'wrap',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 13,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#C4C4C4',
-        alignItems: 'center',
-        marginVertical:10,
-        paddingHorizontal:25,
-      },
-      anotherview: {
-        // backgroundColor:'pink',
-        // borderRadius:50,
-        // width:20,
-        // height:20,
-        // paddingVertical:5
-      },
-      text_two: {
-        color: '#A4A4A4',
-        fontWeight: '500',
-        fontSize:16,
-        marginLeft:10
-      },
-      linearGradient: {
-        width: '100%',
-        marginTop: 20,
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderRadius: 5,
-        paddingVertical: 5,
-      },
-      buttonText: {
-        fontSize: 18,
-        //fontFamily: 'Gill Sans',
-        textAlign: 'center',
-        margin: 10,
-        color: '#ffffff',
-        fontWeight:'600'
-        // backgroundColor: 'transparent',
-      },
-
-
-})
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  icon_view_one: {
+    marginTop: 10,
+    flex: 0.25,
+    //backgroundColor:'red'
+  },
+  innertext: {
+    fontSize: 22,
+    fontWeight: '500',
+    textAlign: 'center',
+    maxWidth: 210,
+    alignSelf: 'center',
+  },
+  innertextt: {
+    fontSize: 22,
+    fontWeight: '500',
+    textAlign: 'center',
+    maxWidth: 250,
+    alignSelf: 'center',
+  },
+  view_two: {
+    marginTop: 20,
+    flex: 0.8,
+    paddingHorizontal: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  inner_view_two: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 15,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#C4C4C4',
+    marginVertical: 8,
+    paddingHorizontal: 22,
+    marginHorizontal: 8,
+  },
+  anotherview: {
+    // backgroundColor:'pink',
+    // borderRadius:50,
+    // width:20,
+    // height:20,
+    // paddingVertical:5
+  },
+  text_two: {
+    color: '#A4A4A4',
+    fontWeight: '500',
+    fontSize: 16,
+    marginLeft: 10,
+  },
+  linearGradient: {
+    width: '100%',
+    marginTop: 20,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+    paddingVertical: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    //fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    fontWeight: '600',
+    // backgroundColor: 'transparent',
+  },
+});
 
 export default Questionairefive;
--- src/components/Questionaires/Questionairesix.js
+++ src/components/Questionaires/Questionairesix.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import { SafeAreaView,StyleSheet,View,Text,TouchableOpacity,} from 'react-native';
 import {ChevronLeftIcon,Progress, ScrollView} from 'native-base'
 import LinearGradient from 'react-native-linear-gradient';
 
 const Questionairesix = (props) => {
+
+  const [data, setData] = useState([
+    {
+      id: 1,
+      text: 'Opera',
+    },
+    {
+      id: 2,
+      text: 'Afrobeats',
+    },
+    {
+      id: 3,
+      text: 'Musicals',
+    },
+    {
+      id: 4,
+      text: 'Orchestra',
+    },
+    {
+      id: 5,
+      text: 'Chamber Music',
+    },
+    {
+      id: 6,
+      text: 'Folk',
+    },
+    {
+      id: 7,
+      text: 'Spiritual',
+    },
+    {
+      id: 8,
+      text: 'Gospel',
+    },
+    {
+      id: 9,
+      text: 'Jazz',
+    },
+    {
+      id: 10,
+      text: 'Rock',
+    },
+    {
+      id: 11,
+      text: 'Hard rock',
+    },
+    {
+      id: 12,
+      text: 'Rap',
+    },
+    {
+      id: 13,
+      text: 'Dance',
+    },
+    {
+      id: 14,
+      text: 'Blues',
+    },
+    {
+      id: 15,
+      text: 'Hip-Hop',
+    },
+  ]);
+
+  const handleClick = (item) =>{
+    const updateValues = data.map((content)=>{
+     if (item?.id === content?.id)
+     return {...content, check: !item?.check}
+     else return content;
+    })
+    setData(updateValues)
+   }
+   console.log(data,'data')
   return (
     <SafeAreaView style={styles.MainContainer}>
       <ScrollView>
@@ -23,107 +96,16 @@ const Questionairesix = (props) => {
           </Text>
         </View>
         <View style={styles.view_two}>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Opera</Text>
-            </View>
-            <View
-              style={[
-                styles.inner_view_two,
-                {backgroundColor: '#FF0000', borderColor: '#FFFFFF'},
-              ]}>
-              <Text style={[styles.text_two, {color: '#FFFFFF'}]}>
-                Afrobeats
-              </Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Musicals</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Orchestra</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Chamber Music</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Folk</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Spiritual</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Gospel</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Jazz</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View
-              style={[
-                styles.inner_view_two,
-                {backgroundColor: '#FF0000', borderColor: '#FFFFFF'},
-              ]}>
-              <Text style={[styles.text_two, {color: '#FFFFFF'}]}>Rock</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Hard rock</Text>
-            </View>
-            <View
-              style={[
-                styles.inner_view_two,
-                {backgroundColor: '#FF0000', borderColor: '#FFFFFF'},
-              ]}>
-              <Text style={[styles.text_two, {color: '#FFFFFF'}]}>Rap</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-            }}>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Dance</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Blues</Text>
-            </View>
-            <View style={styles.inner_view_two}>
-              <Text style={styles.text_two}>Hip-Hop</Text>
-            </View>
-          </View>
+          {data.map(item => {
+            return (
+              <TouchableOpacity onPress={() => handleClick(item)}>
+                <View style={item.check ? [styles.inner_view_two,{backgroundColor:'#FF0000',borderColor:'#FFFFFF'}] : styles.inner_view_two }>
+                  <Text style={styles.text_two}>{item.text}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+          
           <LinearGradient
             colors={['#D72D79', '#9264F2']}
             start={{x: 0, y: 0}}
@@ -131,7 +113,6 @@ const Questionairesix = (props) => {
             style={styles.linearGradient}>
             <TouchableOpacity
               onPress={() => {
-                // console.log(props.navigation, 'e..');
                 props.navigation.navigate('questionariesseven');
               }}>
               <Text style={styles.buttonText}>Next</Text>
@@ -143,67 +124,66 @@ const Questionairesix = (props) => {
   );
 }
 
-const styles=StyleSheet.create({
-    MainContainer:{
-    flex:1,
-    backgroundColor:'#FFFFFF'
-    },
-    icon_view_one: {
-        marginTop:10,
-        flex: 0.2,
-        //backgroundColor:'red'
-      },
-      innertext: {
-        fontSize: 22,
-        fontWeight: '500',
-        textAlign: 'center',
-        maxWidth:300,
-        alignSelf:'center'
-        
-      },
-      view_two: {
-        flex: 0.8,
-        paddingHorizontal:20,
-        marginTop:20
-       //backgroundColor: 'red',
-      },
-      inner_view_two: {
-        backgroundColor: '#A4A4A4',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 21,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#C4C4C4',
-        alignItems: 'center',
-        marginVertical:10,
-        paddingHorizontal:25,
-      },
-      text_two: {
-        color: '#FFFFFF',
-        fontWeight: '500',
-        fontSize:16
-      },
-      linearGradient: {
-        width: '100%',
-        marginTop: 20,
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderRadius: 5,
-        paddingVertical: 5,
-      },
-      buttonText: {
-        fontSize: 18,
-        //fontFamily: 'Gill Sans',
-        textAlign: 'center',
-        margin: 10,
-        color: '#ffffff',
-        fontWeight:'600'
-        // backgroundColor: 'transparent',
-      },
-
-
-})
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  icon_view_one: {
+    marginTop: 10,
+    flex: 0.2,
+  },
+  innertext: {
+    fontSize: 22,
+    fontWeight: '500',
+    textAlign: 'center',
+    maxWidth: 300,
+    alignSelf: 'center',
+  },
+  view_two: {
+    marginTop: 20,
+    flex: 0.8,
+    paddingHorizontal: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    //backgroundColor: 'red',
+  },
+  inner_view_two: {
+    backgroundColor: '#A4A4A4',
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 16,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#C4C4C4',
+    marginVertical: 9,
+    paddingHorizontal: 20,
+    marginHorizontal: 8,
+  },
+  text_two: {
+    color: '#FFFFFF',
+    fontWeight: '500',
+    fontSize: 16,
+  },
+  linearGradient: {
+    width: '100%',
+    marginTop: 20,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+    paddingVertical: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    //fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    fontWeight: '600',
+    // backgroundColor: 'transparent',
+  },
+});
 
 export default Questionairesix;
