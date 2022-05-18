fb993a4c3a6723c20ae6b76c9e76f50fb0bd33b8
Monika <monika.recraft@gmail.com>
2022-05-18 17:49:51 +0530

    ui fixes in verification and setting screen

--- src/components/ForgotPassword/Verification.js
+++ src/components/ForgotPassword/Verification.js
@@ -8,99 +8,135 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
-import {VStack, FormControl, Input, Button} from 'native-base';
+import {
+  VStack,
+  FormControl,
+  Input,
+  Button,
+  ScrollView,
+  ChevronLeftIcon,
+} from 'native-base';
 import LinearGradient from 'react-native-linear-gradient';
 
-const Verification = () => {
+const Verification = props => {
   const image = require('../../assets/bg_pattern.png');
-  const ArrowIcon = require('../../assets/Vector_4.png');
   return (
     <SafeAreaView style={styles.MainContainer}>
-      <ImageBackground source={image} style={{width: '100%', height: '100%'}}>
-        <View style={{flex: 0.05}}></View>
-        <View style={styles.firstview}>
-          <Image
-            source={ArrowIcon}
-            style={{width: 16, height: 16, color: '#9D9D9D'}}
-            alt="Alternate Text"></Image>
-          <Text style={[styles.text, {marginLeft: 100}]}>Get Verified</Text>
-        </View>
-        <View style={styles.secondview}>
-          <Text style={styles.secondtext}>
-            Carry out the free eharmony SMS verification now to receive SMS
-            verified symbol for your profile!
-          </Text>
-        </View>
-        <View style={styles.viewthree}>
-          <VStack space={3}>
-            <FormControl mt={4}>
-              <FormControl.Label>
-                <Text style={styles.inlinetext}>Enter your mobile number</Text>
-              </FormControl.Label>
-            
-              <View style={{marginTop:10}}>
-              <Input  py="4" backgroundColor="#F8F8F8" borderRadius="10" style={{borderTopLeftRadius:10,}} rightElement={
-                <TouchableOpacity  style={{backgroundColor:'#FF0000',paddingHorizontal:15,paddingVertical:19,borderRadius:6,}}><Text style={{alignSelf:'center',color:'#FFFFFF',fontSize:15,fontWeight:'600'}}>Send OTP</Text></TouchableOpacity>
-              }/>
-              
+      <ScrollView>
+        <ImageBackground
+          source={image}
+          style={{width: '100%', height: '100%', paddingTop: 30}}>
+          <View style={{flex: 0.05, backgroundColor: 'red'}}></View>
+          <TouchableOpacity
+            style={{marginHorizontal: 10, height: 30, width: 30, zIndex: 10}}
+            onPress={() => {
+              props.navigation.goBack();
+            }}>
+            <ChevronLeftIcon style={{}} iconName="chevron-left" size="5" />
+          </TouchableOpacity>
+
+          <Text style={styles.text}>Get Verified</Text>
+
+          <View style={styles.secondview}>
+            <Text style={styles.secondtext}>
+              Carry out the free eharmony SMS verification now to receive SMS
+              verified symbol for your profile!
+            </Text>
           </View>
-            </FormControl>
-            
-            <FormControl mt={5}>
-              <FormControl.Label>
-                <Text style={styles.inlinetext}>OTP</Text>
-              </FormControl.Label>
-              <Input backgroundColor="#F8F8F8" py="4" mt={2} borderRadius="10" />
-            </FormControl>
-          </VStack>
-          <LinearGradient
-            colors={['#D72D79', '#9264F2']}
-            start={{x: 0, y: 0}}
-            end={{x: 0, y: 1}}
-            style={styles.linearGradient}>
-            <TouchableOpacity
-              onPress={() => {
-                // console.log(props.navigation, 'e..');
-                // props.navigation.navigate('signupthree');
-              }}>
-              <Text style={styles.buttonText}>Start the SMS verification</Text>
-            </TouchableOpacity>
-          </LinearGradient>
-        </View>
-      </ImageBackground>
+          <View style={styles.viewthree}>
+            <VStack space={3}>
+              <FormControl mt={4}>
+                <FormControl.Label>
+                  <Text style={styles.inlinetext}>
+                    Enter your mobile number
+                  </Text>
+                </FormControl.Label>
+
+                <View style={{marginTop: 10}}>
+                  <Input
+                    py="4"
+                    backgroundColor="#F8F8F8"
+                    borderRadius="10"
+                    style={{borderTopLeftRadius: 10}}
+                    rightElement={
+                      <TouchableOpacity
+                        style={{
+                          backgroundColor: '#FF0000',
+                          paddingHorizontal: 15,
+                          paddingVertical: 19,
+                          borderRadius: 6,
+                        }}>
+                        <Text
+                          style={{
+                            alignSelf: 'center',
+                            color: '#FFFFFF',
+                            fontSize: 15,
+                            fontWeight: '600',
+                          }}>
+                          Send OTP
+                        </Text>
+                      </TouchableOpacity>
+                    }
+                  />
+                </View>
+              </FormControl>
+
+              <FormControl mt={5}>
+                <FormControl.Label>
+                  <Text style={styles.inlinetext}>OTP</Text>
+                </FormControl.Label>
+                <Input
+                  backgroundColor="#F8F8F8"
+                  py="4"
+                  mt={2}
+                  borderRadius="10"
+                />
+              </FormControl>
+            </VStack>
+            <LinearGradient
+              colors={['#D72D79', '#9264F2']}
+              start={{x: 0, y: 0}}
+              end={{x: 0, y: 1}}
+              style={styles.linearGradient}>
+              <TouchableOpacity
+                onPress={() => {
+                  // console.log(props.navigation, 'e..');
+                }}>
+                <Text style={styles.buttonText}>
+                  Start the SMS verification
+                </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        </ImageBackground>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   MainContainer: {
     flex: 1,
-    //paddingHorizontal:20,
-    backgroundColor:'#ffffff'
+    backgroundColor: '#ffffff',
   },
   firstview: {
-    flex: 0.1,
-    display: 'flex',
+    flex: 1,
+    // display: 'flex',
     flexDirection: 'row',
-    //justifyContent:'space-evenly',
-    paddingHorizontal: 20,
-    //backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     fontSize: 22,
     fontWeight: '600',
-    marginTop: -5,
-    //alignContent:'center'
+    marginTop: '-10%',
+    textAlign: 'center',
+    color: '#000000',
   },
   secondview: {
+    marginVertical: 30,
     flex: 0.15,
-    //backgroundColor: 'pink',
-    //display:'flex',
+    alignItems: 'center',
     paddingHorizontal: 20,
-
-    //justifyContent:'center',
-    //alignItems:'center',
-    // alignSelf:'center',
-    //alignContent:'center',
   },
   secondtext: {
     color: '#5E5E5E',
@@ -112,7 +148,6 @@ const styles = StyleSheet.create({
   },
   viewthree: {
     flex: 0.45,
-    //backgroundColor: 'blue',
     paddingHorizontal: 20,
   },
   inlinetext: {
@@ -135,7 +170,6 @@ const styles = StyleSheet.create({
     margin: 10,
     fontWeight: '600',
     color: '#ffffff',
-    // backgroundColor: 'transparent',
   },
 });
 
